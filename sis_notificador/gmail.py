2f42baf0bb9725cce8b2ea119c4c1159b9b15b0d
Rodrigo Vieira <rcardoso@gmail.com>
2017-11-04 16:11:55 -0200

    Criação de classe para enviar emails pelo Gmail

---
+++ sis_notificador/gmail.py
@@ -0,0 +1,43 @@
+import smtplib
+import keyring_store
+
+
+class Gmail():
+
+    GMAIL_SMTP_SERVER = "smtp.gmail.com"
+    GMAIL_SMTP_SLL_PORT = 465
+
+    def __init__(self):
+        self.server = smtplib.SMTP_SSL(self.GMAIL_SMTP_SERVER,
+                                       self.GMAIL_SMTP_SLL_PORT)
+
+    def login(self, user, password):
+        self.user = user
+        self.server.ehlo()
+        self.server.login(user, password)
+
+    def close(self):
+        self.server.close()
+
+    def send_mail(self, recipient, subject, text):
+        email_from = self.user
+        email_to = recipient if type(recipient) is list else [recipient]
+        email_message = """From: %s\nTo: %s\nMIME-Version: 1.0\nContent-type: text/html\nSubject: %s\n\n%s
+        """ % (email_from, ", ".join(email_to), subject, text)
+
+        self.server.sendmail(email_from, email_to, email_message)
+
+
+if __name__ == '__main__':
+    gmail_sender = Gmail()
+
+    user = 'rodrigovieirabot'
+    passStore = keyring_store.KeyringStore()
+    password = passStore.get_pass(user)
+    gmail_sender.login(user, password)
+
+    recipient = 'rcardoso@gmail.com'
+    subject = 'teste'
+    text = 'Teste de envio de email'
+    gmail_sender.send_mail(recipient, subject, text)
+    gmail_sender.close()
--- sis_notificador/notificador.py
+++ sis_notificador/notificador.py
@@ -1,30 +1,17 @@
 import requests
 import re
 import datetime
-import smtplib
+import gmail
 import keyring_store
 from bs4 import BeautifulSoup
 
 
-def send_email(user, pwd, recipient, subject, body):
-    import smtplib
-
-    gmail_user = user
-    gmail_pwd = pwd
-    FROM = user
-    TO = recipient if type(recipient) is list else [recipient]
-    SUBJECT = subject
-    TEXT = body
-
-    # Prepare actual message
-    message = """From: %s\nTo: %s\nMIME-Version: 1.0\nContent-type: text/html\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+def send_email(user, pwd, recipient, subject, text):
     try:
-        server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        server.ehlo()
-        server.login(gmail_user, gmail_pwd)
-        server.sendmail(FROM, TO, message)
-        server.close()
+        gmail_sender = gmail.Gmail()
+        gmail_sender.login(user, pwd)
+        gmail_sender.send_mail(recipient, subject, text)
+        gmail_sender.close()
         print('successfully sent the mail')
     except Exception as ex:
         print("failed to send mail")
